ba02105f30927bdc94f4df40031156dce441c5d9
NahomKibreab <nknahom@gmail.com>
2023-09-24 10:39:31 -0600

    Add Array map()

--- main-array-methods.js
+++ main-array-methods.js
@@ -1,4 +1,25 @@
-const names = ["Nahom", "Awet", "Salih", "Selam"];
+const names = ["Nahom", "Awet", "Salih", "Selam"]; // List of names
+const name = "Nahom";
+
+const listOfObject = [
+  { name: "Nahom", age: 12 },
+  { name: "Salih", age: 13 },
+]; // List of objects
+
+const objectName = {
+  name: "Nahom",
+  age: "12",
+  eyeColor: "brown",
+  addLastName: function (lastName) {
+    return this.name + " " + lastName; // Nahom Salih
+  },
+};
+
+console.log("objectName", objectName);
+console.log("objectName.name", objectName.name);
+console.log("objectName.age", objectName.age);
+console.log("objectName.addLastName", objectName.addLastName);
+console.log("objectName.addLastName('Salih')", objectName.addLastName("Salih"));
 
 // for (let i = 0; i < names.length; i++) {
 //   console.log("hi ", names[i]);
@@ -17,9 +38,10 @@ const array = [
   { key: 1, value: 10 },
   { key: 2, value: 20 },
   { key: 3, value: 30 },
-];
+]; // List of objects
 
 const newArrayFromObject = array.map((obj) => {
+  // {key:3, value:30} obj
   return obj.value;
 }); // [10, 20, 30]
 
